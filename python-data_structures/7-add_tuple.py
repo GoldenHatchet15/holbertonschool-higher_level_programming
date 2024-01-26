774c1161557cc5d31590c3a610bcb2c525716bda
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-26 10:34:44 -0400

    task 7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensuring both tuples have at least 2 elements, filling missing ones with 0
+    # Ensuring both tuples have at least 2 elements, filling missing one with 0
     tuple_a += (0, 0)
     tuple_b += (0, 0)
     # Adding the first two elements of each tuple
