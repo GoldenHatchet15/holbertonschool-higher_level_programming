c796e9d699a60cb5c4c212dbe6ec724de4caa63d
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-26 04:05:23 -0400

    task 2 update pycode style

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def main():
     num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
     if num_args == 0:
@@ -12,9 +13,8 @@ def main():
 
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
-    
+
+
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
     main()
-
-
