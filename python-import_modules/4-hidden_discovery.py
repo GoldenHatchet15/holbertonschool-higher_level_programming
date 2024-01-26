15f853caa85b3a97f366022620c6178ad928ff11
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-26 04:57:39 -0400

    task 4 pycodestyle fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import hidden_4
 
+# This function prints all the names defined by the compiled module hidden_4.pyc
+# excluding those starting with '__'.
 def print_names():
     names = dir(hidden_4)
     filtered_names = [name for name in names if not name.startswith('__')]
     for name in sorted(filtered_names):
         print("{}".format(name))
 
+
 if __name__ == "__main__":
-    #Prints all the names defined by the compiled module hidden_4.pyc
+    # Prints all the names defined by the compiled module hidden_4.pyc
     print_names()
