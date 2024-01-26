5c55f1ad846fc148f2fead6118c8d0e9f6b7ed0c
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-26 09:38:15 -0400

    task 1

---
+++ python-data_structures/1-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+element_at = __import__('1-element_at').element_at
+
+if __name__ == "__main__":
+    # function that retrieves an element from a list.
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
