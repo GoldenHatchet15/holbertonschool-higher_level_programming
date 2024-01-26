ff9dfc7bbb322e6e8f7dc5996bb36336fff03d12
GoldenHatchet15 <raphsant20@gmail.com>
2024-01-26 04:00:13 -0400

    task 2 update

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,16 +1,20 @@
-#!/usr/bin/python3    
+#!/usr/bin/python3
 import sys
 
 def main():
-    num_args = len(sys.argv) - 1
+    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
     if num_args == 0:
         print("0 arguments.")
     elif num_args == 1:
-        print("1 arguments:")
+        print("1 argument:")
     else:
         print("{} arguments:".format(num_args))
 
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
-
+    
 if __name__ == "__main__":
+    """Print the number of and list of arguments."""
+    main()
+
+
