fbb9183b2ca6f4346c4e7a8e70b834a4affd5302
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-04 13:18:02 -0400

    task 5

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""Unittests for max_integer([..])."""
+"""Unittest for max_integer([..])"""
 
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
-
 class TestMaxInteger(unittest.TestCase):
-    """Define unittests for max_integer([..])."""
+    """Defines unittests for max_integer([..])."""
 
     def test_ordered_list(self):
         """Test an ordered list of integers."""
@@ -14,11 +13,11 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_unordered_list(self):
         """Test an unordered list of integers."""
-        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
+        self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
     def test_max_at_beginning(self):
         """Test a list with a beginning max value."""
-        self.assertEqual(max_integer([4, 3, 2, 1]), 4)
+        self.assertEqual(max_integer([4, 1, 2, 3]), 4)
 
     def test_empty_list(self):
         """Test an empty list."""
@@ -28,18 +27,13 @@ class TestMaxInteger(unittest.TestCase):
         """Test a list with a single element."""
         self.assertEqual(max_integer([7]), 7)
 
-    def test_floats(self):
-        """Test a list of floats."""
-        self.assertEqual(max_integer([1.53, 6.33, -9.123, 15.2, 6.0]), 15.2)
+    def test_negative_numbers(self):
+        """Test a list with negative numbers."""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
 
-    def test_ints_and_floats(self):
-        """Test a list of ints and floats."""
-        self.assertEqual(max_integer([1.53, 15.5, -9, 15, 6]), 15.5)
-
-    def test_invalid_input(self):
-        """Test lists with invalid input."""
-        self.assertRaises(TypeError, max_integer, ["string", 2, 3, 4])
-        self.assertRaises(TypeError, max_integer, [None, 2, 3, 4])
+    def test_mixed_numbers(self):
+        """Test a list with mixed positive and negative numbers."""
+        self.assertEqual(max_integer([-10, 10, 100, -100]), 100)
 
 if __name__ == '__main__':
     unittest.main()
