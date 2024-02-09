15832e64d45fd6abcafebfbec19ad17146b47b0e
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-09 10:11:57 -0400

    Python - More Classes and Objects

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines an empty class Rectangle.
+
+The Rectangle class is a simple class that does not have any attributes
+or methods implemented yet. It serves as a foundation for future
+implementations where properties and behaviors of a rectangle can be defined.
+"""
+
 class Rectangle:
     """An empty class Rectangle that defines a rectangle"""
     pass
