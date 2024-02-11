2ad9f17f09713cf92304bb8dd1891da09e938830
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-11 03:51:13 -0400

    task 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,19 +2,20 @@
 """Defines a Square class that inherits from Rectangle."""
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """Represents a square."""
-    
+
     def __init__(self, size):
         """Initialize the square."""
-        super().integer_validator("size", size)  # Corrected reference to "size"
+        super().integer_validator("size", size)  # Corrected
         super().__init__(size, size)
         self.__size = size
-    
+
     def area(self):
         """Return the area of the square."""
         return self.__size ** 2
-    
+
     def __str__(self):
         """Return the square description."""
         return f"[Square] {self.__size}/{self.__size}"
