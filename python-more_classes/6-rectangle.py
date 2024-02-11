542f0b4cd8998e786b1c7782f7b1e6857f2ccb80
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-10 22:44:38 -0400

    Task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -25,9 +25,12 @@ class Rectangle:
     __del__(): Print a farewell message when an instance is deleted.
     """
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -100,3 +103,4 @@ class Rectangle:
     def __del__(self):
         """Prints a farewell message when an instance is deleted."""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
