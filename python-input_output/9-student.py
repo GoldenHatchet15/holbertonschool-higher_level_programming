5177e7eadb442cdddade26879e9fd8c6051810ab
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-11 05:13:24 -0400

    task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,11 +3,12 @@
 Class Student that defines a student
 """
 
+
 class Student:
     """Defines a student"""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a Student instance with first name, last name, and age"""
+        """Initializes Student instance with first name, last name, and age"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
