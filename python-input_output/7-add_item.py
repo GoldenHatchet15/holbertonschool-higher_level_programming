c4d922f8d1224a963a9eb04e129de40d8dcbae98
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-11 05:01:57 -0400

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,37 +1,17 @@
 #!/usr/bin/python3
-"""
-This module extends the functionality of previous tasks to implement a script
-that dynamically adds command-line arguments to a persistent list stored in
-a JSON file ("add_item.json").
+"""This module adds all arguments to a Python list and save them to a file."""
 
-Utilizing the previously defined functions 'save_to_json_file' and
-'load_from_json_file' from modules 'save_to_json_file' and
-'load_from_json_file' respectively, it reads the existing list from the file,
-appends the new items (arguments given to the script), and saves the updated
-list back into the same JSON file.
-
-If "add_item.json" does not exist, it will be created on the first run. This
-approach provides a simple way to accumulate data over time from script
-executions without losing the previous state.
-"""
 
 import sys
-# Import the necessary functions from their respective modules.
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
 
-# The JSON file where the list will be stored and updated.
-filename = "add_item.json"
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    # Attempt to load the existing data from the JSON file.
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    # If the file doesn't exist, start with an empty list.
-    items = []
-
-# Extend the current list with command-line arguments (excluding the script name).
-items.extend(sys.argv[1:])
-
-# Save the updated list back to the JSON file, preserving the additions.
-save_to_json_file(items, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
