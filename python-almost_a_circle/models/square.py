77ac4f7f2a99d520960ec0d03e53bd7a90e45f8d
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-15 06:04:37 -0400

    task 12

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -32,22 +32,8 @@ class Square(Rectangle):
     def __str__(self):
         """Return the string representation of the Square."""
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
-
-#    def update(self, *args, **kwargs):
-#        """Update the Square attributes."""
-#        attrs = ['id', 'size', 'x', 'y']
-#        if args and len(args) > 0:
-#            for attr, value in zip(attrs, args):
-#                if hasattr(self, attr):
-#                    setattr(self, attr, value)
-#        else:
-#            for key, value in kwargs.items():
-#                if hasattr(self, key):
-#                    setattr(self, key, value)
-
-     
-                        
-      def update(self, *args, **kwargs):
+    
+    def update(self, *args, **kwargs):
         """Update the Square.
         Args:
             *args (ints): New attribute values.
