f05312af02c4fb159c046698fec0bb94b07c468c
GoldenHatchet15 <raphsant20@gmail.com>
2024-02-15 04:57:57 -0400

    task 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -92,17 +92,61 @@ class Rectangle(Base):
 #        attributes = ['id', 'width', 'height', 'x', 'y']
 #        for attr, value in zip(attributes, args):
 #            setattr(self, attr, value)
+    
+#    def update(self, *args, **kwargs):
+#        """Update the class
+#        Rectangle by assigning an argument to each attribute."""
+#        attributes = ['id', 'width', 'height', 'x', 'y']
+#        if args and len(args) > 0:
+#            for attr, value in zip(attributes, args):
+#                setattr(self, attr, value)
+#        elif kwargs:
+#            for key, value in kwargs.items():
+#                if key in attributes:
+#                    setattr(self, key, value)
 
+    def update(self, *args, **kwargs):
+        """Update the Rectangle.
+        
+         Args:
+         *args (ints): New attribute values.
+                - 1st argument represents id attribute
+                - 2nd argument represents width attribute
+                - 3rd argument represent height attribute
+                - 4th argument represents x attribute
+                - 5th argument represents y attribute
+            **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
 
-     def update(self, *args, **kwargs):
-         """Update the class
-         Rectangle by assigning an argument to each attribute."""
-         attributes = ['id', 'width', 'height', 'x', 'y']
-         if args and len(args) > 0:
-             for attr, value in zip(attributes, args):
-                 setattr(self, attr, value)
-         elif kwargs:
-             for key, value in kwargs.items():
-                 if key in attributes:
-                     setattr(self, key, value)
-
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
