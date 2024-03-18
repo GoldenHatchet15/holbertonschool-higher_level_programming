70978e38054564c9bd558fb64c19acc64a4d86e5
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-18 15:38:13 -0400

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Check if the user exists and create it if not, then set the password
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
 -- Grant all privileges to the user on all databases and tables
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
