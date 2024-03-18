0663f2c06bdb35d89ff966f662e29e9c5d365217
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-18 15:40:23 -0400

    task 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,4 @@
 -- Grant all privileges to the user on all databases and tables
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
