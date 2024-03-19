926bb72d7ce6d9643ea5aeb0161bccf73d7a75d4
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 16:25:29 -0400

    task 0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,31 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""
+This module lists all states from the database `hbtn_0e_0_usa`.
+It takes three arguments: mysql username, mysql password, and database name.
+The results are sorted in ascending order by states.id.
+"""
+
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    try:
-        conn = MySQLdb.connect(host="localhost",
-                               port=3306,
-                               user=sys.argv[1],
-                               passwd=sys.argv[2],
-                               db=sys.argv[3],
-                               charset="utf8")
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM states ORDER BY id ASC")
-        query_rows = cur.fetchall()
-        for row in query_rows:          
-            print(row)
-    except Exception as e:
-        print("Error: ", e)
-    finally:
-        if conn:
-            conn.close()
+    # Connect to the MySQL database
+    conn = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Execute the query to select all states
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+
+    # Fetch all the results of the query
+    query_rows = cur.fetchall()
+
+    # Print each row
+    for row in query_rows:
+        print(row)
+
+    # Close the cursor and connection to the database
+    cur.close()
+    conn.close()
