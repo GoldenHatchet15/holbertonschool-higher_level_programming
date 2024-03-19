0b654198ef3aa8defeb93546737ed605c8a61909
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 15:24:07 -0400

    task 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Lists all Comedy shows from the hbtn_0d_tvshows database
+SELECT ts.title
+FROM tv_shows ts
+JOIN tv_show_genres tsg ON ts.id = tsg.show_id
+JOIN tv_genres tg ON tsg.genre_id = tg.id
+WHERE tg.name = 'Comedy'
+ORDER BY ts.title ASC;
