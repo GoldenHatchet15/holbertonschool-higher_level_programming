ce299bb2494eb385a74a42ba62e998404ef19df3
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 18:50:58 -0400

    task 13

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,11 +10,15 @@ from model_state import Base, State
 import sys
 
 if __name__ == "__main__":
+    # Define the database connection details
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
     # Connect to the given database URL
-    engine = create_engine(
-        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
-        pool_pre_ping=True
-    )
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
 
     # Bind the engine to the metadata of the Base class
     Base.metadata.create_all(engine)
