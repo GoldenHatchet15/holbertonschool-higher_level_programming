b4fde05ca511cf90b06d88dbc14aee15202a3d83
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 14:58:17 -0400

    task 9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Lists all the cities of California
 -- from the hbtn_0d_usa database
-SELECT id, name FROM cities
-WHERE state_id = (
-  SELECT id FROM states WHERE name = 'California'
-)
-ORDER BY id ASC;
+SELECT c.`id`, c.`name`, s.`name`
+  FROM `cities` AS c
+       INNER JOIN `states` AS s
+       ON c.`state_id` = s.`id`
+ ORDER BY c.`id`;
