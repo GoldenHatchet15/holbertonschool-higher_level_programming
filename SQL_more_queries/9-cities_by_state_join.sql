bd51392c07b960204331bb170ef5306dcdb0b31c
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 14:54:49 -0400

    task 9

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all the cities of California
+-- from the hbtn_0d_usa database
+SELECT id, name FROM cities
+WHERE state_id = (
+  SELECT id FROM states WHERE name = 'California'
+)
+ORDER BY id ASC;
