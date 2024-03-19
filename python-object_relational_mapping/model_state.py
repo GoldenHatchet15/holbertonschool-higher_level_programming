9cabd5304ea8977f33d67ffcdccd8938fd5dc797
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 17:28:58 -0400

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -13,8 +13,9 @@ Base = declarative_base()
 
 # Define the State class
 class State(Base):
-
     """Definition of class State that links to the states table"""
+
+    
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
