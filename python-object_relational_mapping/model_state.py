48f2400f637a4277afbdc2c8245e3ac2e4a32b0d
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 17:30:28 -0400

    task 6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,11 +11,12 @@ from sqlalchemy import Column, Integer, String
 # Start link class to table in database
 Base = declarative_base()
 
+
 # Define the State class
 class State(Base):
     """Definition of class State that links to the states table"""
 
-    
+
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
