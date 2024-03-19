730ac1f4758013c6990dca12c6ddc26805dabeb3
GoldenHatchet15 <raphsant20@gmail.com>
2024-03-19 19:09:59 -0400

    task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,11 +10,11 @@ from model_city import City
 import sys
 
 if __name__ == "__main__":
-    engine = create_engine(
-        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}',
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+    '''Create an engine that connects to the database hbtn_0e_14_usa with'''
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
 
     Session = sessionmaker(bind=engine)
     session = Session()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -4,8 +4,8 @@ This module contains the class definition of a City.
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
+
 
 class City(Base):
     """
@@ -15,6 +15,6 @@ class City(Base):
     - has id, name, and state_id attributes
     """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
